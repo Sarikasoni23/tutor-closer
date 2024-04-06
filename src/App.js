31e0c3ab66dcb7badf449524f935d8fab05786f4
Sarikasoni23 <87684515+Sarikasoni23@users.noreply.github.com>
2024-04-06 17:57:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Marquee from 'react-fast-marquee';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <><div className="App">
+      <h1>Tutors from</h1>
     </div>
+    <div>
+      <Marquee>
+        <div>
+          <img src="https://tutorprod.wpenginepowered.com/wp-content/uploads/2020/01/sydney-conservatorium-of-music.png" alt="" />
+        </div>
+        <div>
+          <img src="https://tutorprod.wpenginepowered.com/wp-content/uploads/2020/01/university-of-melbourne-logo.png" alt="" />
+        </div>
+        <div>
+          <img src="https://tutorprod.wpenginepowered.com/wp-content/uploads/2020/01/macquarie-unversity-logo.png" alt="" />
+        </div>
+        <div>
+          <img src="https://tutorprod.wpenginepowered.com/wp-content/uploads/2020/01/usyd-logo.png" alt="" />
+        </div>
+        <div>
+          <img src="https://tutorprod.wpenginepowered.com/wp-content/uploads/2020/01/unsw-logo.png" alt="" />
+        </div>
+        <div>
+          <img src="https://tutorprod.wpenginepowered.com/wp-content/uploads/2020/01/penn-logo.png" alt="" />
+        </div>
+        <div>
+          <img src="https://tutorprod.wpenginepowered.com/wp-content/uploads/2020/01/cornell-university-logo.png" alt="" />
+        </div>
+        <div>
+          <img src="https://tutorprod.wpenginepowered.com/wp-content/uploads/2020/01/university-of-cambridge-logo.png" alt="" />
+        </div>
+        <div>
+          <img src="https://tutorprod.wpenginepowered.com/wp-content/uploads/2020/01/university-of-oxford-logo.png" alt="" />
+        </div>
+        <div>
+          <img src="https://tutorprod.wpenginepowered.com/wp-content/uploads/2020/01/harvard-university-logo.png" alt="" />
+        </div>
+      </Marquee>
+    </div></>
   );
 }
 
